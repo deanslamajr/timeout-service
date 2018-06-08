330c7c1fe40adfc9f848b655a2064b1a38e5276d
Dean Slama <dslama@luc.id>
2018-06-08 09:27:35 -0600

    added dependencies and made PORT set from env vars

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express')
+const nconf = require('nconf')
+
+const envConfig = nconf.argv().env()
+const PORT = nconf.get('PORT') || 1111
+
 var app = express()
 
 const data = { counts: 0 }
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('*', (req, res) => {
   // don't respond... ever!
   data.counts++
   console.log(`${data.counts} received a request...`)
 })
 
-app.listen(1111, () => console.log('timeout-service listening on port 1111'))
+app.listen(PORT, () => console.log(`timeout-service listening on port ${PORT}`))
